32d6edadf5d3712792b879cdcdd4b9076ec0444b
xbeco <pivstone@gmail.com>
2017-10-04 02:49:56 +0800

    fix bug

--- chat/views.py
+++ chat/views.py
@@ -16,5 +16,6 @@ def receiver(request):
     body = six.text_type(request.body)
     data = dict(urllib.parse.parse_qsl(six.text_type(body)))
     LOG.info(data)
-    settings.ES.index(index=settings.INDEX_NAME, doc_type=data["b'Event"], body=data)
+    if "b'Event" in data:
+        settings.ES.index(index=settings.INDEX_NAME, doc_type=data["b'Event"], body=data)
     return HttpResponse()
