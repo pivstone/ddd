bd46a072f92685d37ebbaea91238fe4dc04475a6
xbeco <pivstone@gmail.com>
2017-10-04 01:11:19 +0800

    add debug info

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
+
 from django.http import HttpResponse
 
 from ddd import settings
 
+LOG = logging.getLogger(__name__)
+
 
 def receiver(request):
+    print ("==="+request.body)
     settings.ES.index(index=settings.INDEX_NAME, doc_type=settings.INDEX_NAME, body=request.body)
     return HttpResponse()
