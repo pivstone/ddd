53dd5b420116bc77413ad76411813c24489a6284
xbeco <pivstone@gmail.com>
2017-10-04 01:34:20 +0800

    add dbug

--- chat/views.py
+++ chat/views.py
@@ -15,5 +15,6 @@ LOG = logging.getLogger(__name__)
 def receiver(request):
     body = six.text_type(request.body)
     data = dict(urllib.parse.parse_qsl(six.text_type(body)))
+    LOG.info(data)
     settings.ES.index(index=settings.INDEX_NAME, doc_type=data["Event"], body=request.body)
     return HttpResponse()
